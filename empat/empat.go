a6337fa9ab70f596501620c530ac95e015487527
Yohanes <yohanesdahlan@gmail.com>
2024-01-08 18:11:22 +0700

    tambahan spasi nomer 4

--- empat/empat.go
+++ empat/empat.go
@@ -8,7 +8,7 @@ func main() {
 	for i := printSegitiga; i >= 1; i-- {
 		hasil := ""
 		for j := 1; j <= i; j++ {
-			hasil += fmt.Sprint(j)
+			hasil += fmt.Sprint(" ",j)
 		}
 		fmt.Println(hasil)
 	}
